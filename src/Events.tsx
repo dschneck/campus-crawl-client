b49016b5829c93d6c59932e46767444f0825329f
dschneck <david.schneckster@hotmail.com>
2023-04-22 19:05:46 -0400

    better

--- src/Events.tsx
+++ src/Events.tsx
@@ -8,24 +8,46 @@ const Events : Component = () =>
     const [privateEvents] = createResource({id: user().id, type:"private"} as eventArgs, getEvents);
     const [rsoEvents] = createResource({id: user().id, type:"rso"} as eventArgs, getEvents);
 
-    const RsoEvent = (event: Event) => <li class="bg-rsoColor">{event.name}</li>;
-    const PublicEvent = (event: Event) => <li class="bg-publicColor">{event.name}</li>;
-    const PrivateEvent = (event: Event) => <li class="bg-privateColor">{event.name}</li>;
+    const RsoEvent = (event: Event) => {
+        return (<li class="bg-rsoColor">
+                    <h1 class="text-center text-xanthous">{event.name}</h1>
+                    <p>{`Description: ${event.description}`}</p>
+                    <p>{`Time: ${event.startTime} to ${event.endTime}`}</p>
+                </li>);
+        }
 
+    const PublicEvent = (event: Event) => {
+        return (<li class="bg-publicColor">
+                    <h1 class="text-center text-xanthous">{event.name}</h1>
+                    <p>{`Description: ${event.description}`}</p>
+                    <p>{`Time: ${event.startTime} to ${event.endTime}`}</p>
+                </li>);
+        }
+
+    const PrivateEvent = (event: Event) => {
+        return (<li class="bg-privateColor">
+                    <h1 class="text-center text-xanthous">{event.name}</h1>
+                    <p>{`Description: ${event.description}`}</p>
+                    <p>{`Time: ${event.startTime} to ${event.endTime}`}</p>
+                </li>);
+        }
     return (
         <div class="bg-white p-10">
             <h1 class="text-chestnut text-xl mx-auto p-2 text-center">Events</h1>
             <ul class="overflow-auto list-none divide-y divide-white md:divide-y-8">
+                <h1 class="text-center">Public Events</h1>
                 <For
                 each={publicEvents()}>
                     {(event) => PublicEvent(event)}
                 </For>
 
+                <h1 class="text-center">Private Events</h1>
                 <For
                 each={privateEvents()}>
                     {(event) => PrivateEvent(event)}
                 </For>
 
+                <h1 class="text-center">RSO Events</h1>
                 <For
                 each={rsoEvents()}>
                     {(event) => RsoEvent(event)}
--- src/RSO.tsx
+++ src/RSO.tsx
@@ -6,7 +6,7 @@ import JoinRso from "./JoinRso";
 const RSO : Component = () => {
     return (
         <div class="bg-white mx-auto p-12">
-            <h1 class=" text-chestnut text-xl mx-auto p-4  text-center">RSO</h1>
+            <h1 class=" text-chestnut text-xl mx-auto p-4 text-center">RSO</h1>
         <div class="flex">
             <RsoForm />
             <div>
